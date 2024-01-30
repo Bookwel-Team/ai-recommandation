34e86e9716e683e60f11b5f1a3f823f921ded575
Ylan <hei.nalisoa@gmail.com>
2024-01-30 22:12:17 +0300

    feat: add model for book

--- service/models.py
+++ service/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+    file_link = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
