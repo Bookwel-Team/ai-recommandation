299b800855a6292e1828ba6cfea9af61f1e33bca
Ylan <hei.nalisoa@gmail.com>
2024-01-30 22:21:15 +0300

    feat: userprofile for user

--- service/models.py
+++ service/models.py
@@ -10,3 +10,19 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class UserProfile(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+
+    STATUS_CHOICES = [
+        ('ADMIN', 'Admin'),
+        ('CLIENT', 'Client'),
+    ]
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
