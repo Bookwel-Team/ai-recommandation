8398b24b4c0e5360929a07df96f8c6cd77bf4124
Ylan <hei.nalisoa@gmail.com>
2024-02-06 16:33:02 +0300

    feat: urls

--- service/urls.py
+++ service/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from service import ajax
+from service.ajax import get_recommendations
 
 urlpatterns = [
-   #path("recommandation/", ajax.recommandation, name="recommandation"),
+   path("(?P<user_id>[0-9]+)/recommandation/", get_recommendations, name="get_recommendations"),
 ]
