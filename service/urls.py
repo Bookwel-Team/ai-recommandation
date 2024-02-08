6a8ff79144813359522cc785e99fa5e0fdfaf707
Ylan <hei.nalisoa@gmail.com>
2024-02-08 12:02:22 +0300

    remove: remove unused

--- service/urls.py
+++ service/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
 from service import ajax
-from service.ajax import get_recommendations, extract_info_from_pdf
+from service.ajax import get_recommendations
 
 urlpatterns = [
    path("recommandation/", get_recommendations, name="get_recommendations"),
-   path("pdf/", extract_info_from_pdf, name="extract_info_from_pdf"),
 ]
